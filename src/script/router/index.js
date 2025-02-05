2f028ec5f1fca9d8ed7719aff07babf91c421934
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-05 15:39:33 +0100

    Updating folder structure and router function
    Also decided not to use Vite for now. Uninstalled it again

--- src/script/router/index.js
+++ src/script/router/index.js
@@ -1,32 +1,48 @@
 // This function controls which JavaScript file is loaded on which page
 export default async function router(pathname = window.location.pathname) {
   switch (pathname) {
-    case '/':
+    case '/': // Home logged-in users
       await import('./views/home.js');
       break;
-    case '/auth/login':
+    case '/public': // Home non-logged-in users
+      await import('./views/publicHome.js');
+      break;
+    case '/public/listings': // Listings non-logged-in users (categories page)
+      await import('./views/listingsBrowse.js');
+      break;
+    case '/auth/login': // Login
       await import('./views/login.js');
       break;
-    case '/auth/register':
+    case '/auth/register': // Register
       await import('./views/register.js');
       break;
-    case '/profile':
+    case '/profile': // Profile logged-in users
       await import('./views/profile.js');
       break;
-    case '/profile/edit':
-      await import('./views/profileEdit.js');
+    case '/profile/update': // Update profile logged-in users
+      await import('./views/profileUpdate.js');
       break;
-    case '/listing/create':
+    case '/listings/create': // Create new listing logged-in users
       await import('./views/listingCreate.js');
       break;
-    case '/listing':
+    case '/listing/single': // View listing. Logged in users can place bids, non-logged-in users will not get this option
       await import('./views/listingView.js');
       break;
-    case '/category':
-      await import('./views/category.js');
-      break;
-    case '/listings':
-      await import('./views/listingsBrowse.js');
+    case '/listings/category': // Listings per category for all users
+      const category = pathname.split('/').pop();
+      const validCategories = [
+        'sport',
+        'fashion',
+        'interior',
+        'art',
+        'decor',
+        'vintage',
+      ];
+      if (validCategories.includes(category)) {
+        await import(`./views/listingsPerCategory.js`);
+      } else {
+        await import('./views/notFound.js');
+      }
       break;
     default:
       await import('./views/notFound.js');
