d8873a4482e2b3dd52a51bf7d93e3ae0c4877c30
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-09 01:30:56 +0100

    added mjs to content tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./**/*.{html,js,ts}', '!./node_modules/**/*'],
+  content: ['./**/*.{html,mjs,js,ts}', '!./node_modules/**/*'],
   theme: {
     extend: {
       fontFamily: {
