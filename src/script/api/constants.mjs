611e3192b2bbdde765365dc1c8611b9cc7dcd203
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-10 14:57:10 +0100

    updating  scriptfolder structure again😅
    Adding files and folders to script

---
+++ src/script/api/constants.mjs
@@ -0,0 +1,9 @@
+export const API_BASE = 'https://v2.api.noroff.dev'
+
+export const API_AUTH = `${API_BASE}/auth`
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`
+
+export const API_AUCTION = `${API_BASE}/auction/listings`
