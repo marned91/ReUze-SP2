55901cd95be43c34efbd7377733733fefb7a5c6e
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-25 13:42:09 +0100

    adding alert for bidding
    if user credit is less then bid

--- src/script/utils/handleBid.mjs
+++ src/script/utils/handleBid.mjs
@@ -1,8 +1,11 @@
 import { placeBid } from '../api/bid.mjs'
 import { fetchSingleListing } from '../api/listings.mjs'
+import { fetchProfileData } from '../api/profile.mjs'
 
 export async function handleBid(listingId, bidAmount, displaySingleListing) {
   const token = localStorage.getItem('token')
+  const userInfo = JSON.parse(localStorage.getItem('user'))
+  const username = userInfo?.name
 
   if (!token) {
     alert('You must be logged in to place a bid.')
@@ -16,6 +19,13 @@ export async function handleBid(listingId, bidAmount, displaySingleListing) {
   }
 
   try {
+    const profile = await fetchProfileData(username)
+    const availableCredits = profile.credits
+    if (numericBidAmount > availableCredits) {
+      alert('You do not have enough credits to place this bid.')
+      return
+    }
+
     const listing = await fetchSingleListing(listingId) // Get the current listing
     const currentBid =
       listing.bids.length > 0 ? listing.bids[listing.bids.length - 1].amount : 0
