7815b61933d8eff58aa1ada0ff8d10181d76b287
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-27 22:46:00 +0100

    testing with path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   appType: 'mpa',
@@ -10,18 +11,24 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       input: {
-        home: resolve(__dirname, 'index.html'), // Home logged-in users
-        publicHome: resolve(__dirname, 'public/index.html'), // Home non-logged-in users
-        publicCategories: resolve(__dirname, 'public/categories/index.html'), //Categories for non-logged-in users
-        listingsPerCategory: resolve(__dirname, 'listings/index.html'), //Listings per category
-        login: resolve(__dirname, 'auth/login/index.html'), // Login
-        register: resolve(__dirname, 'auth/register/index.html'), // Register
-        profile: resolve(__dirname, 'profile/index.html'), // Profile logged-in users
-        profileUpdate: resolve(__dirname, 'profile/update/index.html'), // Profile update
-        profilePurchases: resolve(__dirname, 'profile/purchases/index.html'), // Profile purchases
-        listingCreate: resolve(__dirname, 'listings/create/index.html'), // Create new listing
-        listingView: resolve(__dirname, 'listings/view/index.html'), // View listing
-        listingsSearch: resolve(__dirname, 'listings/search/index.html'), //Search results
+        home: path.resolve(__dirname, 'index.html'), // Home logged-in users
+        publicHome: path.resolve(__dirname, 'public/index.html'), // Home non-logged-in users
+        publicCategories: path.resolve(
+          __dirname,
+          'public/categories/index.html',
+        ), //Categories for non-logged-in users
+        listingsPerCategory: path.resolve(__dirname, 'listings/index.html'), //Listings per category
+        login: path.resolve(__dirname, 'auth/login/index.html'), // Login
+        register: path.resolve(__dirname, 'auth/register/index.html'), // Register
+        profile: path.resolve(__dirname, 'profile/index.html'), // Profile logged-in users
+        profileUpdate: path.resolve(__dirname, 'profile/update/index.html'), // Profile update
+        profilePurchases: path.resolve(
+          __dirname,
+          'profile/purchases/index.html',
+        ), // Profile purchases
+        listingCreate: path.resolve(__dirname, 'listings/create/index.html'), // Create new listing
+        listingView: path.resolve(__dirname, 'listings/view/index.html'), // View listing
+        listingsSearch: path.resolve(__dirname, 'listings/search/index.html'), //Search results
       },
       output: {
         entryFileNames: 'assets/[name].[hash].js',
