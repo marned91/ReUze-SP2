e3675c87f990f8dee29b4c938e4656de079db58b
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-28 00:09:58 +0100

    trying to fix dist and vite config
    failing.. 🥲

---
+++ dist/assets/authGuard-DA79hxSV.js
@@ -0,0 +1,2 @@
+import{h as o}from"./main-Cbclldai.js";function a(){localStorage.token||(o("You must be logged in to view this page","error"),window.location.href="/auth/login/")}export{a};
+//# sourceMappingURL=authGuard-DA79hxSV.js.map
---
+++ dist/assets/home-ClDqkqYv.js
@@ -0,0 +1,2 @@
+import{a}from"./authGuard-DA79hxSV.js";import{s as r}from"./goToTag-BDAsXgnR.js";import{s as t}from"./searchListings-7oRbkNN2.js";import"./main-Cbclldai.js";a();r();t("#search-input","#search-button");
+//# sourceMappingURL=home-ClDqkqYv.js.map
