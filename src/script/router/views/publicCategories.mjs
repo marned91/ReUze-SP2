321182d9e53fbe510d2613317f0159538e63a803
marned91 <152280051+marned91@users.noreply.github.com>
2025-02-28 15:20:16 +0100

    deleted js folders for public
    as public is deleted

--- src/script/router/views/publicCategories.mjs
+++
@@ -1,5 +0,0 @@
-import { setupTagNavigation } from '../../utils/goToTag.mjs'
-import { setUpSearch } from '../../utils/searchListings.mjs'
-
-setupTagNavigation()
-setUpSearch('#search-input', '#search-button')
--- src/script/router/views/publicHome.mjs
+++
@@ -1,3 +0,0 @@
-import { setUpSearch } from '../../utils/searchListings.mjs'
-
-setUpSearch('#search-input', '#search-button')
